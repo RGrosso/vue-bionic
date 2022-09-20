78b4920f749e9afbf6df03abe89a10323f2b96c9
Ryan Grosso <ryangrosso@outlook.com>
2022-09-20 16:56:01 +0100

    Started bionic text method

---
+++ src/utils/bionic-text.ts
@@ -0,0 +1,40 @@
+type SeparatorTags = [string, string]
+
+export type Options = {
+    separatorTags?: SeparatorTags;
+}
+
+const formatText = (text: string, separatorTags: SeparatorTags) => {
+    const halfLengthInt = Math.floor(text.length / 2);
+    let result = "";
+    result += separatorTags[0];
+    result += text.slice(0, halfLengthInt);
+    result += separatorTags[1];
+    result += text.slice(halfLengthInt);
+    return result;
+}
+
+const formatWord = (text: string, separatorTags: SeparatorTags) => {
+    const splitText = text.split("-");
+    if (splitText.length > 1) {
+        return splitText.map(st => formatText(st, separatorTags));
+    }
+
+    return [formatText(text, separatorTags)];
+}
+
+
+export const bionicText = (text: string, options?: Options) => {
+    const separatorTags = options?.separatorTags || ["<strong>", "</strong>"];
+
+    if (!text?.length) {
+        return "";
+    }
+
+    const splitText = text.split(" ");
+    const resultArray: string[] = [];
+
+    splitText.forEach(txt => resultArray.push(...formatWord(txt, separatorTags)));
+
+    return resultArray.join(" ");
+}
