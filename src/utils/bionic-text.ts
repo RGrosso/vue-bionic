8f7ac8b29eb18071e90b9a1353eb40c89a2b0418
Ryan Grosso <ryangrosso@outlook.com>
2022-09-24 09:45:18 +0100

    Corrected prettier for type module package json

--- src/utils/bionic-text.ts
+++ src/utils/bionic-text.ts
@@ -1,28 +1,32 @@
-type SeparatorTags = [string, string]
+type SeparatorTags = [string, string];
 
 export type Options = {
     separatorTags?: SeparatorTags;
-}
+};
 
 const formatText = (text: string, separatorTags: SeparatorTags) => {
-    const halfLengthInt = Math.floor(text.length / 2);
+    let halfLengthInt: number;
+    if (text.length <= 3) {
+        halfLengthInt = 1;
+    } else {
+        halfLengthInt = Math.floor(text.length / 2);
+    }
     let result = "";
     result += separatorTags[0];
     result += text.slice(0, halfLengthInt);
     result += separatorTags[1];
     result += text.slice(halfLengthInt);
     return result;
-}
+};
 
 const formatWord = (text: string, separatorTags: SeparatorTags) => {
     const splitText = text.split("-");
     if (splitText.length > 1) {
-        return splitText.map(st => formatText(st, separatorTags));
+        return splitText.map((st) => formatText(st, separatorTags));
     }
 
     return [formatText(text, separatorTags)];
-}
-
+};
 
 export const bionicText = (text: string, options?: Options) => {
     const separatorTags = options?.separatorTags || ["<strong>", "</strong>"];
@@ -34,7 +38,7 @@ export const bionicText = (text: string, options?: Options) => {
     const splitText = text.split(" ");
     const resultArray: string[] = [];
 
-    splitText.forEach(txt => resultArray.push(...formatWord(txt, separatorTags)));
+    splitText.forEach((txt) => resultArray.push(...formatWord(txt, separatorTags)));
 
     return resultArray.join(" ");
-}
+};
